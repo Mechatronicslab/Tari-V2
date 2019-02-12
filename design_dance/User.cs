f81c982a160693e0a0023dc07576bd0a7bb8cff0
adip28 <adi.permana60@ymail.com>
2019-02-12 18:30:37 +0700

    first commit

---
+++ design_dance/User.cs
@@ -0,0 +1,26 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace design_dance
+{
+    class User
+    {
+        [BsonId]
+        public ObjectId id { get; set; }
+        [BsonElement("username")]
+        public String mUsername { get; set; }
+        [BsonElement("password")]
+        public String mPass { get; set; }
+
+        public User(string username, string password)
+        {
+            this.mUsername = username;
+            this.mPass = password;
+        }
+    }
+}
---
+++ design_dance/f_signin.cs
@@ -0,0 +1,94 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace design_dance
+{
+    public partial class f_signin : Form
+    {
+        static string developmentUri = "mongodb://localhost:27017";
+        static string ProductionUri = "mongodb://maria:maria123@167.205.7.226:27017";
+        public static IMongoDatabase db;
+        public static IMongoClient client;
+        public f_signin()
+        {
+            InitializeComponent();
+            Connect();
+        }
+
+        private void f_signin_Load(object sender, EventArgs e) {
+
+            Connect();
+        }
+
+
+
+        private void b_exit_Click(object sender, EventArgs e)
+        {
+            if (System.Windows.Forms.Application.MessageLoop)
+            {
+                System.Windows.Forms.Application.Exit();
+            }
+            else
+            {
+                System.Environment.Exit(1);
+            }
+        }
+
+        private void b_signin_Click(object sender, EventArgs e)
+        {
+            string username = mUsername.TextName;
+            string password = mPassword.TextName;
+            signIn(username, password);
+        }
+
+        public static void Connect()
+        {
+            client = new MongoClient(developmentUri);
+            db = client.GetDatabase("Tari");
+            bool isMongoLive = db.RunCommandAsync((Command<BsonDocument>)"{ping:1}").Wait(1000);
+            if (isMongoLive)
+            {
+                // connected
+                Console.WriteLine("Koneksi Berhasil");
+            }
+            else
+            {
+                // couldn't connect
+                MessageBox.Show("Koneksi Bermasalah atau Server Mengalami Gangguan !! ");
+                Environment.Exit(0);
+            }
+        }
+
+        public void signIn(string username, string password)
+        {
+            IMongoCollection<User> collection = db.GetCollection<User>("users");
+            var builder = Builders<User>.Filter;
+            var filt = builder.Eq("username", username) & builder.Eq("password", password);
+            var ress = collection.Find(filt).FirstOrDefaultAsync().Result;
+
+            if (ress != null)
+            {
+                /*Home frm = new Home();
+                frm.Show();
+                this.Visible = false;*/
+                MessageBox.Show("Login Sukses !!");
+            }
+            else
+            {
+
+                MessageBox.Show("Username atau Password Salah !!");
+            }
+
+
+        }
+    }
+}
