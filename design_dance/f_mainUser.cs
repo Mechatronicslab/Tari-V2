889b9daff7c1b647490db71f84ce0384da3733a6
JuliSuprapto <gakgaul3@gmail.com>
2019-02-14 02:17:36 +0700

    updated

--- design_dance/f_mainUser.cs
+++ design_dance/f_mainUser.cs
@@ -12,6 +12,12 @@ namespace design_dance
 {
     public partial class f_mainUser : Form
     {
+
+        public string url_a = "C:/Users/gakga/Desktop/videos/01.mp4";
+        public string url_b = "C:/Users/gakga/Desktop/videos/02.mp4";
+        public string url_c = "C:/Users/gakga/Desktop/videos/03.mp4";
+        public string url_d = "C:/Users/gakga/Desktop/videos/04.mp4";
+
         public f_mainUser()
         {
             InitializeComponent();
@@ -37,9 +43,30 @@ namespace design_dance
         }
 
         private void b_tariA_Click(object sender, EventArgs e)
-        {
-            f_userAct A = new f_userAct();
+        {            
+            f_userAct A = new f_userAct(this,url_a);
             A.Show();
+            this.Visible = false;
+        }
+
+        private void b_tariB_Click(object sender, EventArgs e)
+        {
+            f_userAct B = new f_userAct(this, url_b);
+            B.Show();
+            this.Visible = false;
+        }
+
+        private void b_tariC_Click(object sender, EventArgs e)
+        {
+            f_userAct C = new f_userAct(this, url_c);
+            C.Show();
+            this.Visible = false;
+        }
+
+        private void b_tariD_Click(object sender, EventArgs e)
+        {
+            f_userAct D = new f_userAct(this, url_d);
+            D.Show();
             this.Visible = false;
         }
     }
